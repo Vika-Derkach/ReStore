cd476c65868d7df182002e629316bc166b585c13
Vika <queenviktoriyphp@gmail.com>
2021-04-21 21:16:26 +0300

    use Redux in order

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,23 +2,41 @@ const initialState = {
   books: [],
   loading: true,
   error: null,
+  cartItem: [
+    {
+      id: 1,
+      name: "Book 1",
+      count: 3,
+      total: 150,
+    },
+    {
+      id: 2,
+      name: "Book 2",
+      count: 7,
+      total: 70,
+    },
+  ],
+  orderTotal: 220,
 };
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case "FETCH_BOOKS_REQUEST":
       return {
+        ...state,
         books: [],
         loading: true,
         error: null,
       };
     case "FETCH_BOOKS_SUCCESS":
       return {
+        ...state,
         books: action.payload,
         loading: false,
         error: null,
       };
     case "FETCH_BOOKS_FAILURE":
       return {
+        ...state,
         books: [],
         loading: false,
         error: action.payload,
